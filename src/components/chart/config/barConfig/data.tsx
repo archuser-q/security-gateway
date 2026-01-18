bfbebfbc89d62bb259400ff8711b5fe3667d42ed
archuser-q <availableemailaddress123@tutamail.com>
2026-01-18 15:07:22 +0700

    update translation

--- src/components/chart/config/barConfig/data.tsx
+++ src/components/chart/config/barConfig/data.tsx
@@ -9,8 +9,10 @@ import {
     getUpstreamListQueryOptions
 } from "@/apis/hooks"
 import { useQuery } from "@tanstack/react-query";
+import { useTranslation } from "react-i18next";
 
 export const useChartData = () => {
+    const { t } = useTranslation();
     const { data: serviceData } = useQuery(getServiceListQueryOptions({page: 1, page_size: 10}));
     const { data: routeData } = useQuery(getRouteListQueryOptions({page: 1, page_size: 10}));
     const { data: upstreamData } = useQuery(getUpstreamListQueryOptions({page: 1, page_size: 10}));
@@ -20,13 +22,13 @@ export const useChartData = () => {
     const { data: secretData } = useQuery(getSecretListQueryOptions({page:1, page_size: 10}));
     const { data: protoData } = useQuery(getProtoListQueryOptions({page: 1, page_size:10}));
     return[
-        { type: 'Services', value: serviceData?.total ?? 0 },
-        { type: 'Routes', value: routeData?.total ?? 0 },
-        { type: 'Upstreams', value: upstreamData?.total ?? 0 },
-        { type: 'Consumers', value: consumerData?.total ?? 0 },
-        { type: 'SSL', value: sslData?.total ?? 0 },
-        { type: 'Global Rules', value: globalRuleData?.total ?? 0},
-        { type: 'Secrets', value: secretData?.total ?? 0},
-        { type: 'Protos', value: protoData?.total ?? 0 }
+        { type: t('sources.services'), value: serviceData?.total ?? 0 },
+        { type: t('sources.routes'), value: routeData?.total ?? 0 },
+        { type: t('sources.upstreams'), value: upstreamData?.total ?? 0 },
+        { type: t('sources.consumers'), value: consumerData?.total ?? 0 },
+        { type: t('sources.ssls'), value: sslData?.total ?? 0 },
+        { type: t('sources.globalRules'), value: globalRuleData?.total ?? 0},
+        { type: t('sources.secrets'), value: secretData?.total ?? 0},
+        { type: t('sources.protos'), value: protoData?.total ?? 0 }
     ];
 };
